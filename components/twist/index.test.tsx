345bd922a1597d73972da4c66dba6b9e3b376322
LopezDev <aragonlopezjuan87@gmail.com>
2022-06-03 23:42:47 +0200

    feat: config test

--- components/twist/index.test.tsx
+++ components/twist/index.test.tsx
@@ -1,2 +1,12 @@
+import { render, screen } from '@testing-library/react'
 import { Twist } from './index'
+import '@testing-library/jest-dom'
 
+test('dice hola', () => {
+
+    render(<Twist />)
+    const text = screen.getByText('hola')
+
+    expect(text)
+
+})
--- components/twist/index.tsx
+++ components/twist/index.tsx
@@ -1,5 +1,5 @@
 export const Twist = () => {
     return (
-        <p>hoal</p>
+        <p>hola</p>
     )
 }
